bd6633af948773257bc6512d085ec8172e9a8f12
empty-codes <morayotimothy@gmail.com>
2025-09-04 10:09:17 +0100

    Add project files.

---
+++ Data/FlightsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TCSA.WebAPIs.FlightsData.Data
+{
+    public class FlightsDbContext: DbContext
+    {
+        public FlightsDbContext(DbContextOptions options): base(options) 
+        { 
+
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
