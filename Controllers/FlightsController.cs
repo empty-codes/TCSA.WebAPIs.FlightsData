a43b072d7fdab53aa1e4221bae89bfa3ba9a197c
empty-codes <morayotimothy@gmail.com>
2025-09-04 12:15:25 +0100

    Creates controller and endpoints

---
+++ Controllers/FlightsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TCSA.WebAPIs.FlightsData.Services;
+
+namespace TCSA.WebAPIs.FlightsData.Controllers
+{
+    [ApiController]
+    [Route("api/[controller")]
+    public class FlightsController
+    {
+        private readonly IFlightService _flightService;
+        public FlightsController(IFlightService flightService)
+        {
+            _flightService = flightService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Flight>> GetAllFlights()
+        {
+            return Ok(_flightService.GetAllFlights());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Flight> GetFlightById(int id)
+        {
+            return Ok(_flightService.GetFlightById(id));
+        }
+
+        [HttpPost]
+        public ActionResult<Flight> CreateFlight(Flight flight)
+        {
+            return Ok(_flightService.CreateFlight(flight));
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Flight> UpdateFlight(int id, Flight updatedFlight)
+        {
+            return Ok(_flightService.UpdateFlight(id, updatedFlight));
+        }
+
+        [HttpDelete("{id")]
+        public ActionResult<Flight> DeleteFlight(int id)
+        {
+            return Ok(_flightService.DeleteFlight(id));
+        }
+    }
+}
